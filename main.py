e1d3516c1358ef20e353efed879cb98fe0e03622
Shantanu Tripathi <40469481+Daishinkan002@users.noreply.github.com>
2020-06-09 16:33:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,51 +5,53 @@ import pyautogui
 import pyscreenshot as scrot
 import numpy as np
 import matplotlib.pyplot as plt
+import threading
 
 
-w,h = pyautogui.size()
-fourcc = cv2.VideoWriter_fourcc(*"XVID")
-out = cv2.VideoWriter("output.avi",fourcc,20.0,pyautogui.size())
-#print(w,h)
+#w,h = pyautogui.size()
+def screenrec():
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    out = cv2.VideoWriter("output.avi",fourcc,20.0,pyautogui.size())
 
-#cv2.namedWindow("Main")
+    while(True):
+        im = scrot.grab()
+        frame = np.array(im)
+        out.write(frame)
+        if cv2.waitKey(1) == ord('a'):
+            break
+        
+    out.release()
+    print("Done Scr. Recording")
 
-while(True):
-    im = scrot.grab()
-    frame = np.array(im)
-    #print(frame,type(frame))
-    #frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-    cv2.namedWindow('frame', cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty("Main",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
-    
-    cv2.imshow("Main", frame)
-    out.write(frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
-    
-out.release()
-cv2.destroyAllWindows()
+def cam_rec():
+    w,h = pyautogui.size()
+    w_loc = (2*w)//3
+    h_loc = (2*h)//3
+    cap = cv2.VideoCapture(0)
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    out = cv2.VideoWriter("output.avi",fourcc,20.0,pyautogui.size())
 
-'''
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        im = scrot.grab()
+        screen = np.array(im)
+        out.write(screen)
+        print("Yup, cam is running")
+        cv2.namedWindow('Cam',cv2.WINDOW_NORMAL)        # Create a named window
+        cv2.moveWindow('Cam', w_loc,h_loc)
+        cv2.resizeWindow('Cam', 400, 300)
+        cv2.imshow('Cam', frame)
+        out.write(screen) 
+        if cv2.waitKey(1) == ord('q'):
+            break
 
-import numpy as np
-import cv2
-cap = cv2.VideoCapture(0)
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,  480))
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    #frame = cv2.flip(frame, 0)
-    # write the flipped frame
-    out.write(frame)
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
-# Release everything if job is finished
-cap.release()
-out.release()
-'''
+    out.release
+    cap.release()
+    print("Done Camera")
+
+
+
+cam_rec()
