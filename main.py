30de536fc567ad7a6ca74eea964d816080c17775
Shantanu Tripathi <40469481+Daishinkan002@users.noreply.github.com>
2020-09-22 02:55:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
-
 import cv2
 import pyautogui
+import requests
 import pyscreenshot as scrot
 import numpy as np
 import os
@@ -22,30 +21,53 @@ def screenrec():
     print("Done Scr. Recording")
 
 def cam_rec():
+    select = input("Are you taking it from mobile ? ")
     w,h = pyautogui.size()
-    w_loc = (2*w)//3
-    h_loc = (2*h)//3
+    w_loc = w-400
+    #h_loc = (2*h)//3
+    h_loc = 0
+
     cap = cv2.VideoCapture(0)
+    if(select == 'y' or 'Y'):
+        ip = "https://192.168.43.1:8080/shot.jpg"
+
     fourcc = cv2.VideoWriter_fourcc(*"XVID")
     out = cv2.VideoWriter("output.avi",fourcc,20.0,pyautogui.size())
-    cv2.namedWindow('Cam',cv2.WINDOW_NORMAL)        # Create a named window
-    
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
-            break
-        im = scrot.grab()
-        screen = np.array(im)
-        out.write(screen)
-        print("Yup, cam is running")
-        cv2.moveWindow('Cam', w_loc,h_loc)
-        cv2.resizeWindow('Cam', 400, 300)
-        os.system("wmctrl -r Cam -b add,above")
-        cv2.imshow('Cam', frame)
-        out.write(screen) 
-        if cv2.waitKey(1) == ord('q'):
-            break
+    cv2.namedWindow('Cam',cv2.WINDOW_NORMAL)
+    if(select == 'y' or select == 'Y'):
+        while(True):
+            resp = requests.get(ip, verify=False)
+            
+            img_arr = np.array(bytearray(resp.content), dtype=np.uint8)
+            img = cv2.imdecode(img_arr, -1)
+            im = scrot.grab()
+            screen = np.array(im)
+            out.write(screen)
+            print("Yup, cam is running")
+            cv2.moveWindow('Cam', w_loc,h_loc)
+            cv2.resizeWindow('Cam', 400, 300)
+            os.system("wmctrl -r Cam -b add,above")
+            cv2.imshow('Cam', img)
+            out.write(screen)
+            if cv2.waitKey(1) == ord('q'):
+                break
+    else:
+        while cv2.waitKey(1) != 27:
+            ret, frame = cap.read()
+            if not ret:
+                print("Can't receive frame (stream end?). Exiting ...")
+                break
+            im = scrot.grab()
+            screen = np.array(im)
+            out.write(screen)
+            print("Yup, cam is running")
+            cv2.moveWindow('Cam', w_loc,h_loc)
+            cv2.resizeWindow('Cam', 400, 300)
+            os.system("wmctrl -r Cam -b add,above")
+            cv2.imshow('Cam', frame)
+            out.write(screen) 
+            if cv2.waitKey(1) == ord('q'):
+                break
 
     
     out.release
@@ -53,5 +75,5 @@ def cam_rec():
     print("Done Camera")
 
 
-
-cam_rec()
+if __name__ == "__main__":
+    cam_rec()
